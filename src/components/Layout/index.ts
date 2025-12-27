0847b5cfde8ffc5c5c71a7374a4968af67f51ea6
Ahrorbek Abdullayev <ahrorbeksoft@mail.ru>
2025-12-27 14:36:56 +0500

    Export TabbarItem and TabbarItemProps types

--- src/components/Layout/index.ts
+++ src/components/Layout/index.ts
@@ -2,3 +2,5 @@ export type { FixedLayoutProps } from './FixedLayout/FixedLayout';
 export { FixedLayout } from './FixedLayout/FixedLayout';
 export type { TabbarProps } from './Tabbar/Tabbar';
 export { Tabbar } from './Tabbar/Tabbar';
+export { TabbarItem } from 'Tabbar/components/TabbarItem/TabbarItem';
+export type { TabbarItemProps } from 'Tabbar/components/TabbarItem/TabbarItem';
